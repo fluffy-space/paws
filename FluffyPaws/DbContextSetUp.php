854f9abc3e3505e47dd6c92814e7c7a09ca5f925
Ivan Voitovych <col606v@gmail.com>
2025-01-16 14:18:27 +0200

    DbContextSetUp

--- FluffyPaws/DbContextSetUp.php
+++ FluffyPaws/DbContextSetUp.php
@@ -5,6 +5,10 @@ namespace FluffyPaws;
 use Fluffy\Data\Context\DbContext;
 use FluffyPaws\Data\Entities\Blog\BlogPostEntity;
 use FluffyPaws\Data\Entities\Blog\BlogPostEntityMap;
+use FluffyPaws\Data\Entities\Content\PageEntity;
+use FluffyPaws\Data\Entities\Content\PageEntityMap;
+use FluffyPaws\Data\Entities\Media\PictureEntity;
+use FluffyPaws\Data\Entities\Media\PictureEntityMap;
 
 /** @namespaces **/
 // !Do not delete the line above!
@@ -14,6 +18,8 @@ class DbContextSetUp
     public static function configure()
     {
         DbContext::registerEntity(BlogPostEntity::class, BlogPostEntityMap::class);
+        DbContext::registerEntity(PageEntity::class, PageEntityMap::class);
+        DbContext::registerEntity(PictureEntity::class, PictureEntityMap::class);
         /** @insert **/
         // !Do not delete the line above!
     }
