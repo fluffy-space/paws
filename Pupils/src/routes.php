20c98b1b01ca32acd729d4f47bb6b65fcf84bc2c
Ivan Voitovych <col606v@gmail.com>
2025-02-01 03:17:32 +0200

    viewi routes

--- Pupils/src/routes.php
+++ Pupils/src/routes.php
@@ -1,8 +1,9 @@
 <?php
 
+use Pupils\Components\Views\Admin\Dashboard\Dashboard;
+use Pupils\Components\Views\Admin\Page\PageList;
 use Pupils\Components\Views\Auth\Login;
 use Viewi\App;
-use Viewi\Components\Http\Message\Response;
 use Viewi\Router\Router;
 
 /**
@@ -12,3 +13,11 @@ $router = $viewiApp->router();
 
 // Auth
 $router->get('/login', Login::class);
+
+// admin
+$router->lazy('admin', function (Router $router) {
+    $router->section('admin', function (Router $router) {
+        $router->get('/', Dashboard::class);
+        $router->get('/content', PageList::class);
+    });
+});
