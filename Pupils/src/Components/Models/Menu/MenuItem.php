580934bfc4d83464606fcfeb6d03dead6062abd3
Ivan Voitovych <col606v@gmail.com>
2025-02-01 03:16:25 +0200

    MenuItem

---
+++ Pupils/src/Components/Models/Menu/MenuItem.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Pupils\Components\Models\Menu;
+
+class MenuItem
+{
+    public bool $expanded = false;
+    public ?MenuItem $parent = null;
+
+    public function __construct(
+        public string $title,
+        public ?string $url = null,
+        public ?string $icon = null,
+        public array $alternatives = [],
+        public ?string $pattern = null,
+        public ?array $children = null
+    ) {
+        if ($this->children !== null) {
+            for ($i = 0; $i < count($this->children); $i++) {
+                $this->children[$i]->parent = $this;
+            }
+        }
+    }
+}
