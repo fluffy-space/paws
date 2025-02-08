69fb05b0c2caaf20f9f06812f4a509fe1c723f2d
Ivan Voitovych <col606v@gmail.com>
2025-02-08 12:34:50 +0200

    routes

--- Pupils/src/routes.php
+++ Pupils/src/routes.php
@@ -1,8 +1,12 @@
 <?php
 
+use Pupils\Components\Views\Admin\Blog\BlogEdit;
+use Pupils\Components\Views\Admin\Blog\BlogList;
 use Pupils\Components\Views\Admin\Dashboard\Dashboard;
 use Pupils\Components\Views\Admin\Page\PageEdit;
 use Pupils\Components\Views\Admin\Page\PageList;
+use Pupils\Components\Views\Admin\Users\UserEdit;
+use Pupils\Components\Views\Admin\Users\UserList;
 use Pupils\Components\Views\Auth\Login;
 use Viewi\App;
 use Viewi\Router\Router;
@@ -22,5 +26,14 @@ $router->lazy('admin', function (Router $router) {
         $router->get('/content', PageList::class);
         $router->get('/content/create', PageEdit::class);
         $router->get('/content/{id}', PageEdit::class);
+
+        $router->get('/blog', BlogList::class);
+        $router->get('/blog/create', BlogEdit::class);
+        $router->get('/blog/{id}', BlogEdit::class);
+
+        
+        $router->get('/user', UserList::class);
+        $router->get('/user/create', UserEdit::class);
+        $router->get('/user/{id}', UserEdit::class);
     });
 });
