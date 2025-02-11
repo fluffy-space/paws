888a9f117e409f0e31556e31ddba9dda75d11a5a
Ivan Voitovych <col606v@gmail.com>
2025-02-11 11:52:48 +0200

    EmailService

--- FluffyPaws/Services/Emails/EmailService.php
+++ FluffyPaws/Services/Emails/EmailService.php
@@ -5,6 +5,8 @@ namespace FluffyPaws\Services\Emails;
 use FluffyPaws\Services\Localization\LocalizationService;
 use Exception;
 use Fluffy\Swoole\Task\TaskManager;
+use Pupils\Components\Emails\Users\ActivateUserEmail;
+use Pupils\Components\Emails\Users\ResetPasswordEmail;
 use ReflectionException;
 use SharedPaws\Models\Auth\UserViewModel;
 use Viewi\App;
@@ -48,65 +50,5 @@ class EmailService
     public function getSendPasswordResetEmail(UserViewModel $user, $verificationCode)
     {
         return $this->viewiApp->engine()->render(ResetPasswordEmail::class, ['user' => $user, 'verificationCode' => $verificationCode]);
-    }
-
-    // NewMemberEmail
-    /**
-     * 
-     * @param UserViewModel $user 
-     * @param EmailAttachment[] $attachments 
-     * @return void 
-     */
-    public function dispatchNewMemberEmail(UserViewModel $user, array $attachments)
-    {
-        $this->tasks->dispatch($this->sendNewMemberEmail(...), $user, $attachments);
-    }
-
-    /**
-     * 
-     * @param UserViewModel $user 
-     * @param EmailAttachment[] $attachments 
-     * @return void 
-     * @throws Exception 
-     * @throws ReflectionException 
-     */
-    public function sendNewMemberEmail(UserViewModel $user, array $attachments)
-    {
-        $html = $this->getNewMemberEmail($user);
-        $this->connector->send($user->Email, $this->localization->localize('email.new-member.title'), $html->body, "{$user->FirstName} {$user->LastName}", $this->localization->localize('email.new-member.title'), $attachments);
-    }
-
-    public function getNewMemberEmail(UserViewModel $user)
-    {
-        return $this->viewiApp->engine()->render(NewMemberEmail::class, ['user' => $user]);
-    }
-
-    // NewMemberAdminEmail
-    /**
-     * 
-     * @param UserViewModel $user 
-     * @return void 
-     */
-    public function dispatchNewMemberAdminEmail(UserViewModel $user, string $email)
-    {
-        $this->tasks->dispatch($this->sendNewMemberAdminEmail(...), $user, $email);
-    }
-
-    /**
-     * 
-     * @param UserViewModel $user 
-     * @return void 
-     * @throws Exception 
-     * @throws ReflectionException 
-     */
-    public function sendNewMemberAdminEmail(UserViewModel $user, string $email)
-    {
-        $html = $this->getNewMemberAdminEmail($user);
-        $this->connector->send($email, $this->localization->localize('email.new-member-admin.title'), $html->body, $this->localization->localize('email.new-member-admin.manager-name'), $this->localization->localize('email.new-member-admin.title'));
-    }
-
-    public function getNewMemberAdminEmail(UserViewModel $user)
-    {
-        return $this->viewiApp->engine()->render(NewMemberAdminEmail::class, ['user' => $user]);
     }
 }
