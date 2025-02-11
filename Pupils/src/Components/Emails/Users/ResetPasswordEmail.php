8066c2f7712f1ee60d0fc5f1dbb3ce9b05a08cad
Ivan Voitovych <col606v@gmail.com>
2025-02-11 11:53:16 +0200

    email templates

---
+++ Pupils/src/Components/Emails/Users/ResetPasswordEmail.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Pupils\Components\Emails\Users;
+
+use SharedPaws\Models\Auth\UserViewModel;
+use Viewi\Components\BaseComponent;
+use Viewi\Components\Config\ConfigService;
+
+class ResetPasswordEmail extends BaseComponent
+{
+    public static bool $_noBrowser = true;
+
+    public string $baseUrl = '/';
+    public UserViewModel $user;
+    public string $verificationCode;
+
+    public function __construct(UserViewModel $user, string $verificationCode, ConfigService $configService)
+    {
+        $this->user = $user;
+        $this->verificationCode = $verificationCode;
+        $this->baseUrl = $configService->get('baseUrl');
+    }
+}
