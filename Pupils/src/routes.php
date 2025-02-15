2d5e6928eab51d09f306bfd9f5ead2fa0c40af4c
Ivan Voitovych <col606v@gmail.com>
2025-02-15 21:57:08 +0200

    Pupils routes

--- Pupils/src/routes.php
+++ Pupils/src/routes.php
@@ -6,6 +6,8 @@ use Pupils\Components\Views\Admin\Dashboard\Dashboard;
 use Pupils\Components\Views\Admin\EmailTemplates\PreviewEmailPage;
 use Pupils\Components\Views\Admin\Localization\LanguageEdit;
 use Pupils\Components\Views\Admin\Localization\LanguageList;
+use Pupils\Components\Views\Admin\MenuItems\MenuItemEdit;
+use Pupils\Components\Views\Admin\MenuItems\MenuItemList;
 use Pupils\Components\Views\Admin\Page\PageEdit;
 use Pupils\Components\Views\Admin\Page\PageList;
 use Pupils\Components\Views\Admin\Users\UserEdit;
@@ -33,7 +35,7 @@ $router->lazy('admin', function (Router $router) {
         $router->get('/blog', BlogList::class);
         $router->get('/blog/create', BlogEdit::class);
         $router->get('/blog/{id}', BlogEdit::class);
-        
+
         $router->get('/user', UserList::class);
         $router->get('/user/create', UserEdit::class);
         $router->get('/user/{id}', UserEdit::class);
@@ -43,5 +45,10 @@ $router->lazy('admin', function (Router $router) {
         $router->get('/language/{id}', LanguageEdit::class);
 
         $router->get('/email-templates', PreviewEmailPage::class);
+
+        // menu items
+        $router->get('/menu/{area}', MenuItemList::class);
+        $router->get('/menu/{area}/create', MenuItemEdit::class);
+        $router->get('/menu/{area}/{id}', MenuItemEdit::class);
     });
 });
