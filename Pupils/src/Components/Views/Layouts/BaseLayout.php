a6aa963dbcb424e1fadfdc620ba2fda3a5fbdab6
Ivan Voitovych <col606v@gmail.com>
2025-05-28 10:34:01 +0000

    BaseLayout

---
+++ Pupils/src/Components/Views/Layouts/BaseLayout.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Pupils\Components\Views\Layouts;
+
+use Pupils\Components\Services\Layouts\LayoutService;
+use Viewi\Components\BaseComponent;
+use Viewi\Components\Config\ConfigService;
+
+class BaseLayout extends BaseComponent
+{
+    public string $title = 'Viewi';
+    public ?string $description = null;
+    public ?string $keywords = null;
+    public string $pirschKey = '';
+
+    public function __construct(public LayoutService $layout, ConfigService $configService)
+    {
+        $this->pirschKey = $configService->get('pirschKey');
+    }
+}
