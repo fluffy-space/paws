ede68885073ecd2f6174d00ef7469b31cbda25de
Ivan Voitovych <col606v@gmail.com>
2025-07-05 07:41:11 +0000

    NotFoundBlock

--- Pupils/src/Components/Views/NotFound/NotFoundBlock.php
+++ Pupils/src/Components/Views/NotFound/NotFoundBlock.php
@@ -3,8 +3,26 @@
 
 namespace Pupils\Components\Views\NotFound;
 
+use SharedPaws\Models\Content\PageModel;
 use Viewi\Components\BaseComponent;
+use Viewi\Components\Http\HttpClient;
 
 class NotFoundBlock extends BaseComponent
 {
+    public ?PageModel $page = null;
+    public bool $ready = false;
+
+    public function __construct(
+        private HttpClient $http
+    ) {}
+
+    public function init()
+    {
+        $this->http->get("/api/content?path=404")
+            ->then(function (?PageModel $page) {
+                $this->page = $page;
+            }, function () {}, function() {
+                $this->ready = true;
+            });
+    }
 }
