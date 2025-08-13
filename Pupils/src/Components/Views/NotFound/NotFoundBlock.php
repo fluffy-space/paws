890278652d95ae70eaed5cd57eb067b41e11ae55
Ivan Voitovych <col606v@gmail.com>
2025-08-13 17:57:31 +0300

    NotFoundBlock

--- Pupils/src/Components/Views/NotFound/NotFoundBlock.php
+++ Pupils/src/Components/Views/NotFound/NotFoundBlock.php
@@ -5,23 +5,27 @@ namespace Pupils\Components\Views\NotFound;
 
 use SharedPaws\Models\Content\PageModel;
 use Viewi\Components\BaseComponent;
+use Viewi\Components\Config\ConfigService;
 use Viewi\Components\Http\HttpClient;
 
 class NotFoundBlock extends BaseComponent
 {
     public ?PageModel $page = null;
     public bool $ready = false;
+    public string $assetsBaseUrl = '';
 
     public function __construct(
-        private HttpClient $http
+        private HttpClient $http,
+        private ConfigService $config
     ) {}
 
     public function init()
     {
+        $this->assetsBaseUrl = $this->config->get('assetsUrl');
         $this->http->get("/api/content?path=404")
             ->then(function (?PageModel $page) {
                 $this->page = $page;
-            }, function () {}, function() {
+            }, function () {}, function () {
                 $this->ready = true;
             });
     }
